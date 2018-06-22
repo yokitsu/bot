e1b09fcf5b1c22ace2eec567d566dcede8a2080c
Dave R <davetheprogrammer2@gmail.com>
2018-06-22 13:54:29 +0100

    wew

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /// Require modules ///
 const { Client, Collection }   = require('discord.js');
 const { readdirSync, readdir } = require('fs');
-const { blue, red }            = require('chalk');
 const { token }                = require('./conf.json');
 
 /// Initiate the client ///
@@ -30,23 +29,21 @@ for (let i = 0; i < categories.length; i++) {
 
 /// Events ///
 client.on('ready', () => {
-    console.log(blue(`Ready and logged in as ${client.user.username}`));
+    console.log(`\x1b[44mReady and logged in as ${client.user.username}\x1b[0m`);
     client.user.setActivity(`Playing on ${client.guilds.size} guilds with ${client.users.size} users | ${client.settings.defaultPrefix}help`);
 });
 
 client.on('message', (msg) => {
     if(!msg.content.startsWith(client.settings.defaultPrefix) || msg.author.bot) return;
-
     const args = msg.content.slice(client.settings.defaultPrefix.length).split(/ +/);
     const commandName = args.shift().toLowerCase();
     const command = client.commands.find(c => c.name.includes(commandName) || c.aliases.includes(commandName));
     if(!command) return;
-
     try {
         command.execute(client, msg, args);
-        console.log(blue(`${msg.author.tag} ran command ${commandName ? command.name : command.name}`));
+        console.log(`\x1b[44m${msg.author.tag} ran command ${commandName ? command.name : command.name}\x1b[0m`);
     } catch(e) {
-        console.log(red(e.stack));
+        console.log(`\x1b[41m${e.stack}\x1b[0m`);
         msg.channel.send(`Error whilst executing command \`${commandName.slice(client.settings.defaultPrefix.length)}\``);
     }
 });
