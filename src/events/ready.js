07eff4d9de2b5a858a7012eef5fd0ed42acc0553
Ryan <admin@voided.pw>
2018-09-05 14:35:29 +0800

    better

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 module.exports = {
   event: 'ready',
   execute: (bot) => {
-    console.info(`[${bot.user.tag}] Yokitsu has established connection to Discord.`);
-    bot.user.setActivity(`${bot.config.prefix}help || ${bot.guilds.size} Guild${bot.guilds.size > 1 ? "s" : ""}`);
+    console.info(`[${bot.user.tag}] Yokitsu has established connection to Discord`);
+    bot.user.setActivity(`${bot.config.prefix}help | ${bot.guilds.size} guild${bot.guilds.size > 1 ? 's' : ''}`);
   }
-};
+}
