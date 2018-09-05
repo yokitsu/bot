45c555c000eb86f694f3bc029bdcb68aad4e9ffd
Ryan <admin@voided.pw>
2018-09-05 14:38:21 +0800

    better

--- src/events/message.js
+++ src/events/message.js
@@ -15,15 +15,15 @@ module.exports = {
     const cmd = bot.cmds.filter(c => c.command === command || c.aliases.includes(command));
 
     if (cmd.length > 0) {
-      if (cmd[0].guildOnly && msg.channel.type === 'dm') return msg.channel.send(`:x: You must be in a guild to execute the \`${cmd[0].command}\` command.`);
-      if (cmd[0].ownerOnly && !bot.getOP(msg.author.id)) return msg.channel.send(`:x: You must be the bot developers to execute the \`${cmd[0].command}\` command.`);
+      if (cmd[0].guildOnly && msg.channel.type === 'dm') return msg.channel.send(`:x: You must be in a guild to execute the \`${cmd[0].command}\` command`);
+      if (cmd[0].ownerOnly && !bot.getOP(msg.author.id)) return msg.channel.send(`:x: You must be a developer to execute the \`${cmd[0].command}\` command`);
 
       try {
         await cmd[0].execute(bot, msg, args);
       } catch(error) {
-        msg.channel.send(`:x: Command \`${cmd[0].command}\` has errored, the incident has been logged.`);
-        console.error(`[Command/${cmd[0].command}] Command has been errored:\n${error.stack}`);
+        msg.channel.send(`:x: Command \`${cmd[0].command}\` has errored, the incident has been logged`);
+        console.error(`[${cmd[0].command}] Command has been errored:\n${error.stack}`);
       }
     }
   }
-};
+}
