bd9276f2e48d4c774adf317d85436c47bc9e2bb6
August (Chris) <augu@voided.pw>
2019-01-03 21:48:26 -0700

    chore: init typescript rewrite

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// Client imports and instantiation
-const YokitsuClient = require('./structures/Client.js');
-
-new YokitsuClient({ 
-    disableEveryone: true
-}).build(); // Build registries and log into Discord
